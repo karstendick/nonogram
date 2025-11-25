a8606c65df0c8073a16e8458a48dc01c174ce5ba
Joshua Karstendick <joshdick@gmail.com>
2025-11-25 15:06:25 -0500

    cleanup orphaned preview deployments

--- preview/feature-puzzle-selection/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/nonogram/preview/feature-puzzle-selection/sw.js', { scope: '/nonogram/preview/feature-puzzle-selection/' })})}
